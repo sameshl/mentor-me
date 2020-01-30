0a12147356f62bdf013c7d530543c2eb95f207a2
Sarvesh Shinde <SarveshShinde64@gmail.com>
2020-01-31 00:19:31 +0530

    Fixed unifiedtopology

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,8 +5,7 @@ console.log(db)
 const connectDB = async () => {
   try {
     await mongoose.connect(db, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
+      useNewUrlParser: true
     })
     console.log('MongoDB connected!')
   } catch (err) {
