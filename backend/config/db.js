0a6bfbd92e1d6da49013d25ae15bbf67c2526fac
Sarvesh Shinde <SarveshShinde64@gmail.com>
2020-02-01 02:33:48 +0530

    Add mongo atlas

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,9 +6,9 @@ const connectDB = async () => {
   try {
     await mongoose.connect(db, {
       useNewUrlParser: true,
-      useUnifiedTopology: true
+      useUnifiedTopology: false
     })
-    console.log('MongoDB connected!')
+    console.log('MongoDB Atlas connected!')
   } catch (err) {
     console.log(err.message)
     process.exit(1)
