e66617a62f99d96918b1ac3255ee98e9d18a2649
Samesh Lakhotia <samesh.lakhotia@gmail.com>
2020-02-01 10:35:12 +0530

    initial chat

--- backend/index.js
+++ backend/index.js
@@ -2,24 +2,45 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const dotenv = require('dotenv')
 const cors = require('cors')
+
+
+
 // Set up env variables
 dotenv.config()
 
 const connectDB = require('./config/db')
 
 const app = express()
-
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
 // Connect to database
 connectDB()
 
 // Middleware functions
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use(cors())
+app.use(cors()) 
 
 app.use('/api/mentor', require('./routes/mentor'))
 app.use('/api/mentee', require('./routes/mentee'))
+app.use('/', (req, res) => {
+    return res.json(
+    "hi"
+    )
+})
 
+
+io.on("connection", socket => {
+    const { id } = socket.client;
+    console.log(`User connected: ${id}`);
+  });
+  
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+
+const PORTIO = 7000
+
+server.listen(PORTIO, () => console.log(`Listen on *: ${PORTIO}`));
+
+
